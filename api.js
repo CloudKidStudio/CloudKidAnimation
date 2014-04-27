417ab374b4b40b5d2524036cff032928367ba55f
Matt Karl <matt@mattkarl.com>
2014-04-27 15:55:23 -0400

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "cloudkid.Animator",
+        "cloudkid.AnimatorTimeline",
+        "cloudkid.CharacterClip",
+        "cloudkid.CharacterController",
+        "cloudkid.PixiAnimator",
+        "cloudkid.PixiAnimator.AnimTimeline"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
